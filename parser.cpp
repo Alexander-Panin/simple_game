ce7e8a7663760e84549ad4cebe20d8bcd91564c1
Sanek Panin <sanek@Saneks-MacBook-Air.local>
2014-11-23 00:33:38 +0400

    n

--- parser.cpp
+++ parser.cpp
@@ -53,104 +53,89 @@ struct parser_section_t {
 };
 
 struct section_output { };
-void parse_section(const section_output&, token_t& level,token_t& key,token_t& conn) {
-  token_t prev_level = level; token_t tmp_key;
-  while (parse_step(level, key, conn) && level != prev_level) {
+void parse_section(const section_output&, const token_t& sec_level,parser_t& parser) {
+  token_t level, key, conn, tmp_key;
+  while (parser(level, key, conn) && level != sec_level) {
     if (conn == ":") tmp_key = key;
     if (conn == "," || conn == ';') record_result(tmp_key, to_number(key))
   }
 }
 
 struct section_logic { };
-void parse_section(const section_logic&, token_t& level,token_t& key,token_t& conn) {
-  token_t prev_level = level; token_t when;
-  while (parse_step(level, key, conn) && level != prev_level) {
+void parse_section(const section_logic&, const token_t& sec_level, parser_t& parser) {
+  token_t level, key, conn, when;
+  while (parser(level, key, conn) && level != sec_level) {
     bool is_when = false;
     if (key == "relate") constraints.push_back(relate_t());
     else if (key == "when") {
-      parse_step(level, key, conn);
+      if (parser(level, key, conn)) ; else assert(false);
       when = key; is_when=true;
     }
     else if (conn == "<==") {
-      constraints.back().emplace_back( rule { key, is_when ? when : "true" } );
+      constraints.back().emplace_back( rule { key, is_when ? when : "true", &parser});
     }
   }
 }
 
-struct buff_stream; struct numeric_reducer;
-
 struct rule {
   token_t key_;
-  token_t when;
-  bool operator()() {
-    if (lookup(when)) ; else return true;
+  token_t when_;
+  parser_t* p;
+  bool operator()(parser_t* p) {
+    if (lookup(when_)) ; else return true;
     token_t key, conn;
-    buff_stream bs; bs(key, conn);
+    buff_stream bs(p); bs(level, key, conn);
     numeric_reducer nr { bs };
-    return to_number(key_) == nr(key, conn);
+    return to_number(key_) == nr(level, key, conn);
   }
 }
 
+template <typename Op>
 struct buff_stream {
+  Op* next;
   std::vector<std::pair<token_t>> buff;
-  buff_stream() {
+
+  buff_stream(Op* next) : next(next) {
     buff.reserve(16);
     token_t level, key, conn;
-    while (parse_step(level, key, conn)) {
+    while ((*next)(level, key, conn)) {
       buff.push_back(std::make_pair(key, conn));
       if (conn == ';') break;
     }
     std::reverse(begin(buff), end(buff));
   }
-  void operator()(token_t& key, token_t& conn) {
-    assert(buff.size());
+  bool operator()(token_t& level, token_t& key, token_t& conn) {
+    if (buff.size()) ; else return false;
     auto p = buff.back(); buff.pop_back();
     key = p.first; conn = p.second;
+    return true;
   }
 }
 
 struct section_input { };
 struct section_interface { };
-template <typename T>
-void parse_section(const T& x, token_t& level, token_t& key, token_t& conn) {
-  token_t prev_level = level; token_t tmp_key;
-  while (parse_step(level, key, conn) && level != prev_level) {
+template <typename T, typename Op>
+void parse_section(const T& x, token_t& level, token_t&   parser_t& parser) {
+  token_t level, key, conn, tmp_key;
+  while (parser(level, key, conn) && sec_level != level) {
     if (conn == ":") tmp_key = key; continue;
     if (conn == ";") { record(key, value_t()); continue; }
-    numeric_reducer nr { std::bind1st(parse_step, level) };
-    record(tmp_key, nr(key, conn));
+    numeric_reducer nr { &parser };
+    record(tmp_key, nr(level, key, conn));
   }
 }
-
-bool parse() {
-  token_t level, key, conn;
-  std::vector<token_t> keys { "output", "logic", "interface", "input" };
-  std::vector<parser_section_t> funct {
-    parser_section_t { section_output() },
-    parser_section_t { section_logic() },
-    parser_section_t { section_interface() },
-    parser_section_t { section_input() }
-  };
-  while (parse_step(level, key, conn)) {
-    if (key == keys.back()) keys.pop_back(); else return false;
-    funct.back().parse_section(level, key, conn); funct.pop_back();
-  }
-  return true;
-}
-
 
 template <typename Op>
-// Op should provide new key and conn
 struct numeric_reducer
 {
-  Op next;
+  Op* next;
 
-  value_t operator()(token_t& key, token_t& conn) {
+  value_t operator()(token_t& level, token_t& key, token_t& conn) {
     assert(conn != ";");
     value_t key0 = to_number(key, conn);
     numeric_reduce_step ns { key0, conn, value_t() };
     do {
-      if (next(key, conn)) ; else assert(false);
+      if ((*next)(level, key, conn)) ; else assert(false);
       key0 = to_number(key, conn);
     } while (ns(key0, conn));
     return ns.acc_;
@@ -162,7 +147,7 @@ struct numeric_reduce_step
   // require conn = '+' or '*'
   value_t key_; token_t conn_; value_t acc_;
 
-  bool operator()(value_t key, token_t conn)  {
+  bool operator()(value_t key, const token_t& conn)  {
     if (conn_ == '+') acc_ += key_;
     if (conn_ == '*') key *= key_;
     if (conn == ';') { acc_ += key; return false; }
@@ -171,27 +156,49 @@ struct numeric_reduce_step
 }
 
 template <typename I>
-struct parser
+struct parser_t
 // require value_type(I) == token_t
 // && InputIterator(I)
 {
   I f;
   I l;
 
-  bool parse_step(token_t& level, token_t& key, token_t& conn) {
+  bool operator()(token_t& level, token_t& key, token_t& conn) {
     return parse_step0(level) && parse_step0(key) && parse_step0(conn);
   }
 
-  bool parse_step0(token token_t& t) {
+  bool parse_step0(token_t& t) {
     if (f == l) return false;
     t = *f; ++f; return true;
   }
+
 };
+
+template <typename I>
+// require value_type(I) == token_t
+// && InputIterator(I)
+bool parse(I first, I last) {
+  std::vector<token_t> keys { "output", "logic", "interface", "input" };
+  std::vector<const parser_section_t> funct {
+    parser_section_t { section_output() },
+    parser_section_t { section_logic() },
+    parser_section_t { section_interface() },
+    parser_section_t { section_input() }
+  };
+  token_t level, key, conn;
+  auto parser = parser_t { first, last };
+  parser(level, key, conn);
+  while (true) {
+    if (key == keys.back() && conn == ":") keys.pop_back(); else return false;
+    parse_section(funct.back(), level, key, conn, parser); funct.pop_back();
+  }
+  return true;
+}
 
 int main() {
   using it = std::istream_iterator<token_t>;
   it l; it f(std::cin);
-  while (f != l) { }
+  assert(parse(f,l));
 
   return 0;
 }
